64424a71ce8b429c83bf767b1970033da0e7185a
saroyar <yarsaro2001@gmail.com>
2024-01-31 00:58:57 +0530

    icon src change

--- FrontEnd/src/Components/SideBar/SideBar.jsx
+++ FrontEnd/src/Components/SideBar/SideBar.jsx
@@ -6,36 +6,36 @@ const SideBar = () => {
         <div className='sidebar_component'>
             <div className='top_portion'>
                 <div className="logo">
-                    <h2><img src='src\assest\icons\Logomark.svg' /> <span>OrangeFarm</span></h2>
+                    <h2><img src='https://res.cloudinary.com/dwcarlfhd/image/upload/v1706628845/chakr/Logomark_ptq7ks.svg' /> <span>OrangeFarm</span></h2>
                 </div>
                 <div className="search">
-                    <img src='src\assest\icons\search.svg'/>
+                    <img src='https://res.cloudinary.com/dwcarlfhd/image/upload/v1706628847/chakr/search_-_Copy_uspols.svg'/>
                     <form action="">
                         <input type="text" placeholder='search' />
                     </form>
                 </div>
                 <div className="dashboard">
                     <div className="dashbord_component">
-                        <img src="src\assest\icons\dashbord.svg" alt="" /> <span>Dashboard</span>
+                        <img src="https://res.cloudinary.com/dwcarlfhd/image/upload/v1706628840/chakr/dashbord_v9w6vu.svg" alt="" /> <span>Dashboard</span>
                     </div>
                     <div className="dashbord_component">
-                        <img src="src\assest\icons\customers.svg" alt="" /> <span>Customers</span>
+                        <img src="https://res.cloudinary.com/dwcarlfhd/image/upload/v1706628842/chakr/customers_-_Copy_slcgkd.svg" alt="" /> <span>Customers</span>
                     </div>
                     <div className="dashbord_component">
                         <img src='src/assest/icons/allReports.svg' /> <span>All reports</span>
                     </div>
                     <div className="dashbord_component">
-                        <img src='src\assest\icons\geography.svg' /> <span>Geography</span>
+                        <img src='https://res.cloudinary.com/dwcarlfhd/image/upload/v1706628844/chakr/geography_-_Copy_buehfi.svg' /> <span>Geography</span>
                     </div>
                     <div className="dashbord_component">
-                        <img src='src\assest\icons\conversations.svg' /> <span>Conversations</span>
+                        <img src='https://res.cloudinary.com/dwcarlfhd/image/upload/v1706628840/chakr/conversations_-_Copy_at3lzj.svg' /> <span>Conversations</span>
                     </div>
                     <div className="dashbord_component">
 
-                        <img src='src\assest\icons\deals.svg' /> <span>Deals</span>
+                        <img src='https://res.cloudinary.com/dwcarlfhd/image/upload/v1706628842/chakr/deals_a94jaz.svg' /> <span>Deals</span>
                     </div>
                     <div className="dashbord_component">
-                        <img src='src\assest\icons\exports.svg' /> <span>Export</span>
+                        <img src='https://res.cloudinary.com/dwcarlfhd/image/upload/v1706628843/chakr/exports_-_Copy_yy6ugf.svg' /> <span>Export</span>
 
                     </div>
                 </div>
@@ -52,8 +52,8 @@ const SideBar = () => {
                     </div>
                 </div>
                 <div className="settings">
-                    <h3><img src='src\assest\icons\settings.svg' /> <span>Setting</span></h3>
-                    <button type='submit'><img src='src\assest\icons\logout.svg' /> <span>Log out</span></button>
+                    <h3><img src='https://res.cloudinary.com/dwcarlfhd/image/upload/v1706628847/chakr/settings_nfjrfp.svg' /> <span>Setting</span></h3>
+                    <button type='submit'><img src='https://res.cloudinary.com/dwcarlfhd/image/upload/v1706628846/chakr/logout_ztspv4.svg' /> <span>Log out</span></button>
                 </div>
             </div>
         </div>
